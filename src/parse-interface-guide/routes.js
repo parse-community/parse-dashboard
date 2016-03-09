e3a4186cec4e48a414e78b9477ea0bc7ac5d0138
Kevin Old <kevin@kevinold.com>
2016-03-09 10:58:48 -0600

    Use browserHistory from React Router 2 instead of createHashHistory for PIG, per console warning

--- src/parse-interface-guide/routes.js
+++ src/parse-interface-guide/routes.js
@@ -5,13 +5,12 @@
  * This source code is licensed under the license found in the LICENSE file in
  * the root directory of this source tree.
  */
-import createHashHistory from 'history/lib/createHashHistory';
 import PIG               from 'parse-interface-guide/PIG.react';
 import React             from 'react';
-import { Router, Route } from 'react-router';
+import { browserHistory, Router, Route } from 'react-router';
 
 module.exports = (
-<Router history={createHashHistory()}>
+<Router history={browserHistory}>
   <Route path='/' component={PIG} />
   <Route path='/:component' component={PIG} />
 </Router>
