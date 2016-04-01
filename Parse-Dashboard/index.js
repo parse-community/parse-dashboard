fbaaff23e742ef8edb9222339fd95ff89518b502
Florent Vilmart <florentvilmart@me.com>
2016-04-01 12:51:42 -0400

    :construction: Splits express app from express server

--- Parse-Dashboard/index.js
+++ Parse-Dashboard/index.js
@@ -7,11 +7,10 @@
  */
 // Command line tool for npm start
 "use strict"
-const packageJson = require('package-json');
-const basicAuth = require('basic-auth');
 const path = require('path');
 const jsonFile = require('json-file-plus');
 const express = require('express');
+const parseDashboard = require('./app');
 
 const program = require('commander');
 program.option('--appId [appId]', 'the app Id of the app you would like to manage.');
@@ -23,17 +22,6 @@ program.option('--port [port]', 'the port to run parse-dashboard');
 program.option('--allowInsecureHTTP [allowInsecureHTTP]', 'set this flag when you are running the dashboard behind an HTTPS load balancer or proxy with early SSL termination.');
 
 program.parse(process.argv);
-
-const currentVersionFeatures = require('../package.json').parseDashboardFeatures;
-
-var newFeaturesInLatestVersion = [];
-packageJson('parse-dashboard', 'latest').then(latestPackage => {
-  if (latestPackage.parseDashboardFeatures instanceof Array) {
-    newFeaturesInLatestVersion = latestPackage.parseDashboardFeatures.filter(feature => {
-      return currentVersionFeatures.indexOf(feature) === -1;
-    });
-  }
-});
 
 const port = program.port || process.env.PORT || 4040;
 const allowInsecureHTTP = program.allowInsecureHTTP || process.env.PARSE_DASHBOARD_ALLOW_INSECURE_HTTP;
@@ -103,75 +91,7 @@ p.then(config => {
 
   const app = express();
 
-  // Serve public files.
-  app.use(express.static(path.join(__dirname,'public')));
-
-  // Serve the configuration.
-  app.get('/parse-dashboard-config.json', function(req, res) {
-    const response = {
-      apps: config.data.apps,
-      newFeaturesInLatestVersion: newFeaturesInLatestVersion,
-    };
-    const users = config.data.users;
-
-    let auth = null;
-    //If they provide auth when their config has no users, ignore the auth
-    if (users) {
-      auth = basicAuth(req);
-    }
-
-    //Based on advice from Doug Wilson here:
-    //https://github.com/expressjs/express/issues/2518
-    const requestIsLocal =
-      req.connection.remoteAddress === '127.0.0.1' ||
-      req.connection.remoteAddress === '::ffff:127.0.0.1' ||
-      req.connection.remoteAddress === '::1';
-    if (!requestIsLocal && !req.secure && !allowInsecureHTTP) {
-      //Disallow HTTP requests except on localhost, to prevent the master key from being transmitted in cleartext
-      return res.send({ success: false, error: 'Parse Dashboard can only be remotely accessed via HTTPS' });
-    }
-
-    if (!requestIsLocal && !users) {
-      //Accessing the dashboard over the internet can only be done with username and password
-      return res.send({ success: false, error: 'Configure a user to access Parse Dashboard remotely' });
-    }
-
-    const successfulAuth =
-      //they provided auth
-      auth &&
-      //there are configured users
-      users &&
-      //the provided auth matches one of the users
-      users.find(user => {
-        return user.user == auth.name &&
-               user.pass == auth.pass
-      });
-    if (successfulAuth) {
-      //They provided correct auth
-      return res.json(response);
-    }
-
-    if (users || auth) {
-      //They provided incorrect auth
-      res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
-      return res.sendStatus(401);
-    }
-
-    //They didn't provide auth, and have configured the dashboard to not need auth
-    //(ie. didn't supply usernames and passwords)
-    if (requestIsLocal) {
-      //Allow no-auth access on localhost only, if they have configured the dashboard to not need auth
-      return res.json(response);
-    }
-    //We shouldn't get here. Fail closed.
-    res.send({ success: false, error: 'Something went wrong.' });
-  });
-
-  // For every other request, go to index.html. Let client-side handle the rest.
-  app.get('/*', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
-  });
-
+  app.use(parseDashboard(config.data));
   // Start the server.
   app.listen(port);
 
