abb5ac8ffd41e29cb8084fd313ff57842cb329b4
Federico Bacci <fedeb703@gmail.com>
2016-05-21 01:18:09 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+{
+  "apps": [
+    {
+      "serverURL": "your parse server url",
+      "appId": "your app Id",
+      "masterKey": "your master key",
+      "appName": "My Parse App"
+    }
+  ],
+  "users": [
+    {
+      "user":"username",
+      "pass":"password"
+    }
+  ]
+}
