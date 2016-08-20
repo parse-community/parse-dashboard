92d7fa88b84326e32e77cabe2732d390edb8fef9
Robin Reiter <robin7331@gmail.com>
2016-08-20 22:58:23 +0200

    using mount path when mounted as express module (#486)

--- Parse-Dashboard/app.js
+++ Parse-Dashboard/app.js
@@ -17,14 +17,15 @@ packageJson('parse-dashboard', 'latest').then(latestPackage => {
 });
 
 function getMount(req) {
-  let url = req.url;
-  let originalUrl = req.originalUrl;
-  var mountPathLength = req.originalUrl.length - req.url.length;
-  var mountPath = req.originalUrl.slice(0, mountPathLength);
-  if (!mountPath.endsWith('/')) {
-    mountPath += '/';
+  const url = req.url;
+  const originalUrl = req.originalUrl;
+  const mountPath = process.env.MOUNT_PATH || '';
+  const mountPathLength = req.originalUrl.length - req.url.length;
+  let mountPathLocal = req.originalUrl.slice(0, mountPathLength);
+  if (!mountPathLocal.endsWith('/')) {
+    mountPathLocal += '/';
   }
-  return mountPath;
+  return mountPath + mountPathLocal;
 }
 
 function checkIfIconsExistForApps(apps, iconsFolder) {
