1921a7ffa6403b584ed5b82110c27265dc476493
Florent Vilmart <florent@flovilmart.com>
2017-10-28 15:40:27 -0400

    fixup! Better beforeScript

--- scripts/before_script.sh
+++ scripts/before_script.sh
@@ -4,5 +4,5 @@ set -x
 
 test -f Parse-Dashboard/public/bundles/dashboard.bundle.js
 test -f Parse-Dashboard/public/bundles/login.bundle.js
-test -f Parse-Dashboard/public/bundles/sprites.svgs
+test -f Parse-Dashboard/public/bundles/sprites.svg
 test -d Parse-Dashboard/public/bundles/img
