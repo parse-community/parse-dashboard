40a6b726e799663c3b1a4b50a07ba47ee3a8eb86
Florent Vilmart <florent@flovilmart.com>
2017-10-28 15:31:20 -0400

    Better beforeScript

--- scripts/before_script.sh
+++ scripts/before_script.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-test -f Parse-Dashboard/public/bundles/dashboard.bundle.js || exit 1
-test -f Parse-Dashboard/public/bundles/login.bundle.js || exit 1
-test -f Parse-Dashboard/public/bundles/sprites.svg || exit 1
-test -d Parse-Dashboard/public/bundles/img || exit 1
+set -e
+set -x
+
+test -f Parse-Dashboard/public/bundles/dashboard.bundle.js
+test -f Parse-Dashboard/public/bundles/login.bundle.js
+test -f Parse-Dashboard/public/bundles/sprites.svgs
+test -d Parse-Dashboard/public/bundles/img
