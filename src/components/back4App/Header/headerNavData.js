cb5fbd1de24b34080256faeaeeaaa42911840264
André Pesci Cazetto <cazetto.andre@gmail.com>
2017-12-20 17:44:02 -0200

    Changes

--- src/components/back4App/Header/headerNavData.js
+++ src/components/back4App/Header/headerNavData.js
@@ -4,13 +4,12 @@ const headerNavData = {
     {label: 'Dashboard', url: 'https://parse-dashboard.back4app.com/'},
     {label: 'Docs', url: 'http://docs.back4app.com/'},
     {label: 'Community', url: 'https://groups.google.com/forum/#!forum/back4app'},
-    {label: 'Blog', url: 'http://blog.back4app.com/'},
+    {label: 'Blog', url: 'http://blog.back4app.com/'}
   ],
   dropdownItems: [
     {label: 'Account Keys', url:'https://dashboard.back4app.com/classic#/wizard/account-key'},
-    {label: 'Edit Password', url:'https://dashboard.back4app.com/password/change'},
     {label: 'Edit Email', url:'https://dashboard.back4app.com/email/change'},
-    {label: 'Sign Out', url:'https://dashboard.back4app.com/logout'}
+    {label: 'Edit Password', url:'https://dashboard.back4app.com/password/change'}
   ]
 }
 
