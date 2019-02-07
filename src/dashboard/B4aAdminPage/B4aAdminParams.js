63d98477f7a7dc47230b6dd904d7c1fa1a1e2709
alencarlucas <alencarlucaas@gmail.com>
2019-02-07 10:41:03 -0200

    feat: Add new admin params

--- src/dashboard/B4aAdminPage/B4aAdminParams.js
+++ src/dashboard/B4aAdminPage/B4aAdminParams.js
@@ -3,47 +3,233 @@ const MENU_ITEM_CLASS = 'B4aMenuItem'
 const CUSTOM_FIELD_CLASS = 'B4aCustomField'
 const SETTING_CLASS = 'B4aSetting'
 
-const B4aAdminParams = ({ appName }) => ({
+const B4aAdminParams = ({appName}) => ({
   classes: [
     {
       name: SETTING_CLASS,
       rows: [
-        { key: "appName", value: appName }
-      ]
+        {key: 'appName', value: appName},
+      ],
     },
     {
       name: MENU_ITEM_CLASS,
       rows: [
-        { title: "Menu Item", objectClassName: MENU_ITEM_CLASS, relevance: "-1000" },
-        { title: "Custom Field", objectClassName: CUSTOM_FIELD_CLASS, relevance: "-2000" },
-        { title: "Setting", objectClassName: SETTING_CLASS, relevance: "-3000" }
-      ]
+        {
+          title: 'Menu Item',
+          objectClassName: MENU_ITEM_CLASS,
+          relevance: '-1000',
+        },
+        {
+          title: 'Custom Field',
+          objectClassName: CUSTOM_FIELD_CLASS,
+          relevance: '-2000',
+        },
+        {
+          title: 'Setting',
+          objectClassName: SETTING_CLASS,
+          relevance: '-3000',
+        },
+      ],
     },
     {
       name: CUSTOM_FIELD_CLASS,
       rows: [
-        { objectClassName: MENU_ITEM_CLASS, objectClassFieldName: "relevance", title: "Relevance", subType: "", isRequired: false },
-        { objectClassName: MENU_ITEM_CLASS, objectClassFieldName: "objectClassName", title: "Class", subType: "", isRequired: true },
-        { objectClassName: MENU_ITEM_CLASS, objectClassFieldName: "title", title: "Title", subType: "", isRequired: true },
-        { objectClassName: "_User", objectClassFieldName: "emailVerified", title: "Is Email Verified", subType: "", isRequired: false },
-        { objectClassName: "_User", objectClassFieldName: "authData", title: "Auth Data", subType: "", isRequired: false },
-        { objectClassName: "_User", objectClassFieldName: "email", title: "Email", subType: "", isRequired: false },
-        { objectClassName: "_User", objectClassFieldName: "password", title: "Password", subType: "PASSWORD", isRequired: true },
-        { objectClassName: "_User", objectClassFieldName: "username", title: "Username", subType: "", isRequired: true }
-      ]
-    }
+        {
+          objectClassName: '_User',
+          objectClassFieldName: 'username',
+          title: 'Username',
+          isRequired: false,
+          isTableHidden: false,
+          relevance: -1000,
+        },
+        {
+          objectClassName: '_User',
+          objectClassFieldName: 'password',
+          title: 'Password',
+          subType: 'PASSWORD',
+          isRequired: false,
+          isTableHidden: true,
+          relevance: -2000,
+        },
+        {
+          objectClassName: '_User',
+          objectClassFieldName: 'email',
+          title: 'Email',
+          isRequired: false,
+          isTableHidden: false,
+          relevance: -3000,
+        },
+        {
+          objectClassName: '_User',
+          objectClassFieldName: 'emailVerified',
+          title: 'Is Email Verified',
+          isRequired: false,
+          isTableHidden: false,
+          relevance: -4000,
+        },
+        {
+          objectClassName: '_User',
+          objectClassFieldName: 'authData',
+          title: 'Auth Data',
+          isRequired: false,
+          isTableHidden: false,
+          relevance: -5000,
+        },
+        {
+          objectClassName: 'B4aMenuItem',
+          objectClassFieldName: 'title',
+          title: 'Title',
+          isRequired: true,
+          isTableHidden: false,
+          relevance: -1000,
+        },
+        {
+          objectClassName: 'B4aMenuItem',
+          objectClassFieldName: 'objectClassName',
+          title: 'Class',
+          isRequired: true,
+          subType: 'SELECT',
+          options: 'objectClasses.map(objectClass => objectClass.name).sort()',
+          isTableHidden: false,
+          relevance: -2000,
+        },
+        {
+          objectClassName: 'B4aMenuItem',
+          objectClassFieldName: 'relevance',
+          title: 'Relevance2',
+          isRequired: true,
+          defaultValue: '0',
+          isTableHidden: false,
+          relevance: -3000,
+        },
+        {
+          objectClassName: 'B4aCustomField',
+          objectClassFieldName: 'objectClassName',
+          title: 'Class',
+          subType: 'SELECT',
+          isRequired: true,
+          options: 'objectClasses.map(objectClass => objectClass.name).sort()',
+          isTableHidden: false,
+          relevance: -1000,
+        },
+        {
+          objectClassName: 'B4aCustomField',
+          objectClassFieldName: 'objectClassFieldName',
+          title: 'Field',
+          subType: 'SELECT',
+          isRequired: true,
+          options: '(objectClasses.find(objectClass => objectClass.name === objectEntity.objectClassName) || { fields: [] }).fields.map(objectClassField => objectClassField.name).sort()',
+          isTableHidden: false,
+          relevance: -2000,
+        },
+        {
+          objectClassName: 'B4aCustomField',
+          objectClassFieldName: 'title',
+          title: 'Title',
+          isRequired: true,
+          isTableHidden: false,
+          relevance: -3000,
+        },
+        {
+          objectClassName: 'B4aCustomField',
+          objectClassFieldName: 'subType',
+          title: 'Sub Type',
+          subType: 'SELECT',
+          options: '((objectClasses.find(objectClass => objectClass.name === objectEntity.objectClassName) || { fields: [] }).fields.find(objectClassField => objectClassField.name === objectEntity.objectClassFieldName) || { type: \'\' }).type === \'STRING\' ? [\'PASSWORD\', \'SELECT\'] : []',
+          isRequired: false,
+          isTableHidden: false,
+          relevance: -4000,
+        },
+        {
+          objectClassName: 'B4aCustomField',
+          objectClassFieldName: 'isRequired',
+          title: 'Is Required',
+          isRequired: true,
+          defaultValue: 'false',
+          isTableHidden: false,
+          relevance: -8000,
+        },
+        {
+          objectClassName: 'B4aCustomField',
+          objectClassFieldName: 'options',
+          title: 'Options (JS)',
+          isRequired: false,
+          defaultValue: '["Option A", "Option B"]',
+          isTableHidden: true,
+          isFormHidden: 'objectEntity.subType !== \'SELECT\'',
+          relevance: -5000,
+        },
+        {
+          objectClassName: 'B4aCustomField',
+          objectClassFieldName: 'isTableHidden',
+          title: 'Is Table Hidden',
+          isRequired: true,
+          options: '',
+          defaultValue: 'false',
+          isTableHidden: false,
+          relevance: -6000,
+        },
+        {
+          objectClassName: 'B4aCustomField',
+          objectClassFieldName: 'isFormHidden',
+          title: 'Is Form Hidden (JS)',
+          isRequired: false,
+          options: '',
+          isTableHidden: false,
+          relevance: -7000,
+        },
+        {
+          objectClassName: 'B4aCustomField',
+          objectClassFieldName: 'defaultValue',
+          title: 'Default Value',
+          isRequired: false,
+          options: '',
+          isTableHidden: false,
+          relevance: -9000,
+        },
+        {
+          objectClassName: 'B4aCustomField',
+          objectClassFieldName: 'relevance',
+          title: 'Relevance2',
+          isRequired: true,
+          options: '',
+          defaultValue: '0',
+          isTableHidden: false,
+          relevance: -10000,
+        },
+        {
+          objectClassName: 'B4aSetting',
+          objectClassFieldName: 'key',
+          title: 'Key',
+          subType: 'SELECT',
+          isRequired: true,
+          options: ['appName'],
+          isTableHidden: false,
+          relevance: -1000,
+        },
+        {
+          objectClassName: 'B4aSetting',
+          objectClassFieldName: 'value',
+          title: 'Value',
+          isRequired: true,
+          options: '["Option A", "Option B"]',
+          isTableHidden: false,
+          relevance: -2000,
+        },
+      ],
+    },
   ],
   adminRole: ADMIN_ROLE,
   defaultCLP: {
     find: {
-      [`role:${ADMIN_ROLE}`]: true
+      [`role:${ADMIN_ROLE}`]: true,
     },
     get: {},
     create: {},
     update: {},
     delete: {},
-    addField: {}
-  }
+    addField: {},
+  },
 })
 
 export default B4aAdminParams
