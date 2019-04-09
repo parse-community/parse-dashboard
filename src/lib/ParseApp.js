acbbe0a01ece15fd0dc1d32a9c39432b26dacd18
Douglas Muraoka <douglas.muraoka@gmail.com>
2019-04-09 16:53:26 -0300

    refactor: Update index creation API usage

--- src/lib/ParseApp.js
+++ src/lib/ParseApp.js
@@ -882,7 +882,7 @@ export default class ParseApp {
 
   createIndexes() {
     let path = '/parse-app/index';
-    return axios.post(path, { appId: this.applicationId }).catch(err => {
+    return axios.post(path, { appId: this.applicationId, index: { '$**': 'text' } }).catch(err => {
       throw err.response && err.response.data && err.response.data.error ? err.response.data.error : err
     })
   }
