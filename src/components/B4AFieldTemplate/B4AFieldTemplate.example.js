8194e9ffec394c85cb704df43dd2d4576fc13388
alencarlucas <alencarlucaas@gmail.com>
2019-04-12 15:25:38 -0300

    feat: Add B4AFieldTemplate component

---
+++ src/components/B4AFieldTemplate/B4AFieldTemplate.example.js
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2016-present, Parse, LLC
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+import React    from 'react';
+import B4AFieldTemplate    from 'components/B4AFieldTemplate/B4AFieldTemplate.react';
+
+export const component = B4AFieldTemplate;
+
+export const demos = [
+  {
+    render: () => (
+      <B4AFieldTemplate
+        imageSource="https://www.back4app.com/assets/images/illustrations/back4app-platform-desktop.jpg"
+        title='Radio'
+        subtitle='iOS Social Listings Shopping Application'
+        author='xscoder in Full Applications'
+        text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s."
+        link='https://www.back4app.com'
+      />
+    )
+  }
+];
---
+++ src/components/B4AFieldTemplate/B4AFieldTemplate.react.js
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2016-present, Parse, LLC
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+import PropTypes from 'lib/PropTypes';
+import React     from 'react';
+import styles    from 'components/B4AFieldTemplate/B4AFieldTemplate.scss';
+import B4ALabelTemplate    from 'components/B4ALabelTemplate/B4ALabelTemplate.react';
+import B4ADescriptionTemplate    from 'components/B4ADescriptionTemplate/B4ADescriptionTemplate.react';
+
+let B4AFieldTemplate = ({imageSource, title, subtitle, author, text, link}) => {
+  let classes = [styles.field];
+
+  return (
+    <div className={classes.join(' ')}>
+      <div className={styles.left}>
+        <B4ALabelTemplate
+          imageSource={imageSource}
+          title={title}
+          subtitle={subtitle}
+          author={author}
+        />
+      </div>
+      <div className={styles.right}>
+      <B4ADescriptionTemplate
+        text={text}
+        link={link}
+      />
+      </div>
+    </div>
+  );
+};
+
+export default B4AFieldTemplate;
+
+B4AFieldTemplate.propTypes = {};
