a07a1bd34ca96298edf63820a62d76c7239e63de
Douglas Muraoka <douglas.muraoka@gmail.com>
2019-04-30 15:40:38 -0300

    fix: Deploy scripts Node version

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -10,8 +10,8 @@ now=`date '+%Y%m%d%H%M%S'`
 branch=master
 git='~/bin/git-parse-dashboard'
 
-ssh -t -i $b4a_certs_path/$pem $user@$host "sudo su back4app -c 'cp -r ~/scm/parse-dashboard ~/scm/parse-dashboard-$now && . ~/.nvm/nvm.sh && nvm use 9 && cd ~/scm/parse-dashboard && $git reset --hard && $git remote update && $git checkout $branch && $git merge origin/$branch && yarn install --production=false '"
-ssh -t -i $b4a_certs_path/$pem $user@$host "sudo su back4app -c '. ~/.nvm/nvm.sh && nvm use 9 && cd ~/scm/parse-dashboard && sed -i \"s/http:\/\/localhost:4000\/parseapi/https:\/\/dashboard.back4app.com\/parseapi/\" node_modules/parse/lib/browser/settings.js && npm run build'"
+ssh -t -i $b4a_certs_path/$pem $user@$host "sudo su back4app -c 'cp -r ~/scm/parse-dashboard ~/scm/parse-dashboard-$now && . ~/.nvm/nvm.sh && nvm use 10 && cd ~/scm/parse-dashboard && $git reset --hard && $git remote update && $git checkout $branch && $git merge origin/$branch && yarn install --production=false '"
+ssh -t -i $b4a_certs_path/$pem $user@$host "sudo su back4app -c '. ~/.nvm/nvm.sh && nvm use 10 && cd ~/scm/parse-dashboard && sed -i \"s/http:\/\/localhost:4000\/parseapi/https:\/\/dashboard.back4app.com\/parseapi/\" node_modules/parse/lib/browser/settings.js && npm run build'"
 
 curl -X DELETE "https://api.cloudflare.com/client/v4/zones/69ace06909c83213745231d2c6d0fd27/purge_cache" -H "X-Auth-Email: $CF_EMAIL" -H "X-Auth-Key: $CF_KEY" -H "Content-Type: application/json" --data '{"files":[
   "https://parse-dashboard.back4app.com/bundles/dashboard.bundle.js",
@@ -20,4 +20,3 @@ curl -X DELETE "https://api.cloudflare.com/client/v4/zones/69ace06909c8321374523
   "https://parse-dashboard.back4app.com/bundles/quickstart.bundle.js",
   "https://parse-dashboard.back4app.com/bundles/sprites.svg"
   ]}'
-#ssh -t aws_B4ANFS_Homolog "sudo su back4app -c '. ~/.nvm/nvm.sh && nvm use 9 && cd ~/scm/back4app-site && yarn install && npm run build && pm2 reload site'"
