a828ed0dc3816119c5486b94fce73e64bc4c95ce
Douglas Muraoka <douglas.muraoka@gmail.com>
2019-06-27 14:47:40 -0300

    GraphQL API Console not configured state

--- src/dashboard/Data/ApiConsole/GraphQLConsole.react.js
+++ src/dashboard/Data/ApiConsole/GraphQLConsole.react.js
@@ -10,10 +10,25 @@ import PropTypes     from 'lib/PropTypes';
 import React, { Component } from 'react';
 import { Provider } from 'react-redux'
 import { Playground, store } from 'graphql-playground-react';
+import EmptyState from 'components/EmptyState/EmptyState.react';
+import Toolbar from 'components/Toolbar/Toolbar.react';
 
 export default class GraphQLConsole extends Component {
   render() {
     const { applicationId, graphQLServerURL, masterKey } = this.context.currentApp
+    if (!graphQLServerURL) {
+      return (
+        <>
+          <Toolbar section='Core' subsection='GraphQL API Console' />
+          <EmptyState
+            title='GraphQL API Console'
+            description='Please update Parse-Server to version equal or above
+            3.5.0 and define the "graphQLServerURL" on your app configuration
+            in order to use the GraphQL API Console.'
+            icon='info-solid' />
+        </>
+      );
+    }
     const headers = {
       'X-Parse-Application-Id': applicationId,
       'X-Parse-Master-Key': masterKey
