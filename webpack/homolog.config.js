88b037999f4b00261d33f92bf0466fb80fd75b09
Douglas Muraoka <douglas.muraoka@gmail.com>
2019-07-05 11:13:07 -0300

    chore: Split vendors chunk from dashboard

--- webpack/homolog.config.js
+++ webpack/homolog.config.js
@@ -1,5 +1,6 @@
 var configuration = require('./publish.config.js');
 var webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
 
 configuration.plugins.push(
   new webpack.DefinePlugin({
@@ -12,5 +13,24 @@ configuration.plugins.push(
     include: /dashboard.*.*/
   })
 );
+
+configuration.optimization = {
+  splitChunks: {
+    cacheGroups: {
+      commons: {
+        test: /[\\/]node_modules[\\/]/,
+        name: 'vendors',
+        chunks: 'all'
+      }
+    }
+  },
+  minimizer: [
+    new TerserPlugin({
+      cache: true,
+      parallel: true,
+      sourceMap: true // Must be set to true if using source-maps in production
+    })
+  ]
+}
 
 module.exports = configuration;
