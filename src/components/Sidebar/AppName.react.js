730757ed68f7b463288b4468ac8cf08d297bf714
Douglas Muraoka <douglas.muraoka@gmail.com>
2019-09-06 10:51:45 -0300

    fix: Handling app name text-overflow

--- src/components/Sidebar/AppName.react.js
+++ src/components/Sidebar/AppName.react.js
@@ -5,8 +5,10 @@ import styles from 'components/Sidebar/Sidebar.scss';
 const AppName = ({ name, onClick, onPinClick }) => (
   <div>
     <div className={styles.currentApp}>
-      <div className={styles.currentAppName} onClick={onClick}>
-        {name}
+      <div className={styles.appNameContainer} onClick={onClick}>
+        <div className={styles.currentAppName}>
+          {name}
+        </div>
       </div>
       <Pin onClick={onPinClick} />
     </div>
