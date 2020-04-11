bc5b4508a3cdc8b0ca7e52a1942bb291f718ebe8
Nino Crljenec <nino.crljenec@gmail.com>
2020-04-11 11:20:59 +0200

    fix: eslint fixes

--- src/dashboard/Data/Browser/LoginDialog.react.js
+++ src/dashboard/Data/Browser/LoginDialog.react.js
@@ -1,17 +1,17 @@
-import React from "react";
-import ParseApp from "lib/ParseApp";
-import PropTypes from "prop-types";
-import Modal from "components/Modal/Modal.react";
-import LoginRow from "components/LoginRow/LoginRow.react";
-import Notification from "dashboard/Data/Browser/Notification.react";
+import React from 'react';
+import ParseApp from 'lib/ParseApp';
+import PropTypes from 'prop-types';
+import Modal from 'components/Modal/Modal.react';
+import LoginRow from 'components/LoginRow/LoginRow.react';
+import Notification from 'dashboard/Data/Browser/Notification.react';
 
 export default class LoginDialog extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       open: false,
-      username: "",
-      password: ""
+      username: '',
+      password: ''
     };
 
     this.handleOpen = this.handleOpen.bind(this);
@@ -63,7 +63,7 @@ export default class LoginDialog extends React.Component {
           type={Modal.Types.INFO}
           title={currentUser ? "Switch User" : "Browse as User"}
           subtitle={
-            <div style={{ paddingTop: "5px" }}>
+            <div style={{ paddingTop: '5px' }}>
               {currentUser && (
                 <p>
                   Browsing as <strong>{currentUser.get("username")}</strong>
