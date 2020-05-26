16321f016d9fdbaaab706067956329024af716aa
Nino Crljenec <nino.crljenec@gmail.com>
2020-05-26 23:30:26 +0200

    UX changes in main nav bar for browse as user; Toggle new type added - HIDE_LABELS;

--- src/dashboard/Data/Browser/LoginDialog.react.js
+++ src/dashboard/Data/Browser/LoginDialog.react.js
@@ -61,12 +61,12 @@ export default class LoginDialog extends React.Component {
       open && (
         <Modal
           type={Modal.Types.INFO}
-          title={currentUser ? "Switch User" : "Browse as User"}
+          title={currentUser ? 'Switch User' : 'Browse as User'}
           subtitle={
             <div style={{ paddingTop: '5px' }}>
               {currentUser && (
                 <p>
-                  Browsing as <strong>{currentUser.get("username")}</strong>
+                  Browsing as <strong>{currentUser.get('username')}</strong>
                 </p>
               )}
             </div>
