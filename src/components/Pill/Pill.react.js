ec3950d7b5d5ebcc43ad93e100f6d964a1da7f31
sadakchap <aliceprerna@gmail.com>
2021-05-31 18:27:33 +0530

    showing arrow icon if cell has follow link value

--- src/components/Pill/Pill.react.js
+++ src/components/Pill/Pill.react.js
@@ -7,9 +7,27 @@
  */
 import React  from 'react';
 import styles from 'components/Pill/Pill.scss';
+import Icon from "components/Icon/Icon.react";
+
 //TODO: refactor, may want to move onClick outside or need to make onClick able to handle link/button a11y
-let Pill = ({ value, onClick }) => (
-  <span className={[styles.pill, onClick ? styles.action : void(0)].join(' ')} onClick={onClick}>{value}</span>
+let Pill = ({ value, onClick, followClick = false }) => (
+  <span
+    className={[
+      styles.pill,
+      !followClick && onClick ? styles.action : void 0
+    ].join(" ")}
+    onClick={!followClick && onClick ? onClick : null}
+  >
+    {value}
+    {followClick && (
+      <a
+        onClick={onClick}
+        className={!onClick ? styles.disableIconAction : styles.iconAction}
+      >
+        <Icon name="arrow" width={20} height={20} fill="#1669a1" />
+      </a>
+    )}
+  </span>
 );
 
 export default Pill;
