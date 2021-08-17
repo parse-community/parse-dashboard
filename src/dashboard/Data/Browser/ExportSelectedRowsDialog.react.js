8454c56c699f2eced4097ee3cf49c475d95acf16
Manuel <5673677+mtrezza@users.noreply.github.com>
2021-08-17 14:25:04 +0200

    rewording

--- src/dashboard/Data/Browser/ExportSelectedRowsDialog.react.js
+++ src/dashboard/Data/Browser/ExportSelectedRowsDialog.react.js
@@ -27,8 +27,8 @@ export default class ExportSelectedRowsDialog extends React.Component {
       <Modal
         type={Modal.Types.INFO}
         icon='warn-outline'
-        title={this.props.selection['*'] ? 'Export all rows?' : (selectionLength === 1 ? 'Export this row?' : `Export ${selectionLength} rows?`)}
-        subtitle={this.props.selection['*'] ? 'INFO: Export to CSV has a limit of 10,000 rows.' : ''}
+        title={this.props.selection['*'] ? 'Export all rows?' : (selectionLength === 1 ? `Export 1 selected row?` : `Export ${selectionLength} selected rows?`)}
+        subtitle={this.props.selection['*'] ? 'Note: Exporting is limited to the first 10,000 rows.' : ''}
         disabled={!this.valid()}
         confirmText={'Yes export'}
         cancelText={'Never mind, don\u2019t.'}
