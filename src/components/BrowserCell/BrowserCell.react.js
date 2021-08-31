927556356ac16274640d6c19960df636ec0e5433
sadakchap <aliceprerna@gmail.com>
2021-08-31 08:28:08 +0530

    show pill pointer design for array of pointers

--- src/components/BrowserCell/BrowserCell.react.js
+++ src/components/BrowserCell/BrowserCell.react.js
@@ -273,9 +273,13 @@ export default class BrowserCell extends Component {
           const object = new Parse.Object(v.className);
           object.id = v.objectId;
           array.push(
-            <a key={i} href='javascript:;' onClick={onPointerClick.bind(undefined, object)}>
-              <Pill value={v.objectId} />
-            </a>);
+            <Pill
+              key={v.objectId}
+              value={v.objectId}
+              onClick={onPointerClick.bind(undefined, object)}
+              followClick={true}
+            />
+          );
         });
         this.copyableValue = content = <ul>
           { array.map( a => <li>{a}</li>) }
