8fdc662d868c9cdddfe2071dfdc2ae833640591b
dblythy <daniel-blyth@live.com.au>
2021-09-21 00:29:49 +1000

    migrate to graphiql

--- src/dashboard/index.js
+++ src/dashboard/index.js
@@ -14,7 +14,7 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
 require('stylesheets/fonts.scss');
-require('graphql-playground-react/build/static/css/index.css')
+require('graphiql/graphiql.min.css')
 installDevTools(Immutable);
 
 var path = window.PARSE_DASHBOARD_PATH || '/';
