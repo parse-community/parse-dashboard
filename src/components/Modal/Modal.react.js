740069c8d0cc2940ac9af8619747ed6e46d187da
Damian Stasik <visualfanatic@users.noreply.github.com>
2021-10-27 20:21:55 +0200

    fix: use less prominent color for modal resize handle

--- src/components/Modal/Modal.react.js
+++ src/components/Modal/Modal.react.js
@@ -137,7 +137,7 @@ let Modal = (({
             onMouseDown={handleMouseDown}
             >
             <path
-              fill="currentColor"
+              fill="#a5a5b4"
               d="M22,22H20V20H22V22M22,18H20V16H22V18M18,22H16V20H18V22M18,18H16V16H18V18M14,22H12V20H14V22M22,14H20V12H22V14Z"
               />
           </svg>
