parse-community/parse-dashboard

commit 1ff105793fccda33efae1f3742e66b6c6ca213ef

refactor: use new React context API (#1948)

src/dashboard/Data/Browser/Browser.react.js
@@ -36,8 +36,7 @@ import styles                             from 'dashboard/Data/Browser/Browser.s
 import subscribeTo                        from 'lib/subscribeTo';
 import * as ColumnPreferences             from 'lib/ColumnPreferences';
 import { Helmet }                         from 'react-helmet';
-import PropTypes                          from 'lib/PropTypes';
-import ParseApp                           from 'lib/ParseApp';
+import generatePath from 'lib/generatePath';
 
 // The initial and max amount of rows fetched by lazy loading
 const MAX_ROWS_FETCHED = 200;
@@ -153,7 +152,7 @@ class Browser extends DashboardView {
   }
 
   componentWillMount() {
-    const { currentApp } = this.context;
+    const currentApp = this.context;
     if (!currentApp.preventSchemaEdits) {
       this.action = new SidebarAction('Create a class', this.showCreateClass.bind(this));
     }
@@ -168,7 +167,7 @@ class Browser extends DashboardView {
   }
 
   componentWillReceiveProps(nextProps, nextContext) {
-    if (this.context !== nextContext) {
+    if (this.props.params.className !== nextProps.params.className) {
       if (this.props.params.appId !== nextProps.params.appId || !this.props.params.className) {
         this.setState({ counts: {} });
         Parse.Object._clearAllState();
@@ -199,7 +198,7 @@ class Browser extends DashboardView {
       lastMax: -1,
       ordering: ColumnPreferences.getColumnSort(
         false,
-        context.currentApp.applicationId,
+        context.applicationId,
         className,
       ),
       selection: {},
@@ -238,7 +237,7 @@ class Browser extends DashboardView {
         }
         return a.toUpperCase() < b.toUpperCase() ? -1 : 1;
       });
-      history.replace(this.context.generatePath('browser/' + classes[0]));
+      history.replace(generatePath(this.context, 'browser/' + classes[0]));
     }
   }
 
@@ -291,7 +290,7 @@ class Browser extends DashboardView {
   createClass(className) {
     this.props.schema.dispatch(ActionTypes.CREATE_CLASS, { className }).then(() => {
       this.state.counts[className] = 0;
-      history.push(this.context.generatePath('browser/' + className));
+      history.push(generatePath(this.context, 'browser/' + className));
     }).finally(() => {
       this.setState({ showCreateClassDialog: false });
     });
@@ -301,7 +300,7 @@ class Browser extends DashboardView {
     this.props.schema.dispatch(ActionTypes.DROP_CLASS, { className }).then(() => {
       this.setState({showDropClassDialog: false });
       delete this.state.counts[className];
-      history.push(this.context.generatePath('browser'));
+      history.push(generatePath(this.context, 'browser'));
     }, (error) => {
       let msg = typeof error === 'string' ? error : error.message;
       if (msg) {
@@ -313,7 +312,7 @@ class Browser extends DashboardView {
   }
 
   exportClass(className) {
-    this.context.currentApp.exportClass(className).finally(() => {
+    this.context.exportClass(className).finally(() => {
       this.setState({ showExportDialog: false });
     });
   }
@@ -617,7 +616,7 @@ class Browser extends DashboardView {
       this.setState({ computingClassCounts: true });
       for (const parseClass of this.props.schema.data.get('classes')) {
         const [className] = parseClass;
-        counts[className] = await this.context.currentApp.getClassCount(className);
+        counts[className] = await this.context.getClassCount(className);
       }
 
       this.setState({
@@ -683,13 +682,13 @@ class Browser extends DashboardView {
   }
 
   excludeFields(query, className) {
-    let columns = ColumnPreferences.getPreferences(this.context.currentApp.applicationId, className);
+    let columns = ColumnPreferences.getPreferences(this.context.applicationId, className);
     if (columns) {
       columns = columns.filter(clmn => !clmn.visible).map(clmn => clmn.name);
       for (let columnsKey in columns) {
         query.exclude(columns[columnsKey]);
       }
-      ColumnPreferences.updateCachedColumns(this.context.currentApp.applicationId, className);
+      ColumnPreferences.updateCachedColumns(this.context.applicationId, className);
     }
   }
 
@@ -729,7 +728,7 @@ class Browser extends DashboardView {
   }
 
   async fetchRelationCount(relation) {
-    return await this.context.currentApp.getRelationCount(relation);
+    return await this.context.getRelationCount(relation);
   }
 
   fetchNextPage() {
@@ -794,7 +793,7 @@ class Browser extends DashboardView {
       const _filters = JSON.stringify(filters.toJSON());
       const url = `browser/${source}${(filters.size === 0 ? '' : `?filters=${(encodeURIComponent(_filters))}`)}`;
       // filters param change is making the fetch call
-      history.push(this.context.generatePath(url));
+      history.push(generatePath(this.context, url));
     }
   }
 
@@ -806,7 +805,7 @@ class Browser extends DashboardView {
     }, () => this.fetchData(source, this.state.filters));
     ColumnPreferences.getColumnSort(
       ordering,
-      this.context.currentApp.applicationId,
+      this.context.applicationId,
       this.props.params.className
     );
   }
@@ -816,7 +815,7 @@ class Browser extends DashboardView {
     const className = this.props.params.className;
     const entityId = relation.parent.id;
     const relationName = relation.key;
-    return this.context.generatePath(`browser/${className}/${entityId}/${relationName}`);
+    return generatePath(this.context, `browser/${className}/${entityId}/${relationName}`);
   }
 
   setRelation(relation, filters) {
@@ -839,7 +838,7 @@ class Browser extends DashboardView {
         constraint: 'eq',
         compareTo: id
     }]);
-    history.push(this.context.generatePath(`browser/${className}?filters=${encodeURIComponent(filters)}`));
+    history.push(generatePath(this.context, `browser/${className}?filters=${encodeURIComponent(filters)}`));
   }
 
   handlePointerCmdClick({ className, id, field = 'objectId' }) {
@@ -848,7 +847,7 @@ class Browser extends DashboardView {
       constraint: 'eq',
       compareTo: id
     }]);
-    window.open(this.context.generatePath(`browser/${className}?filters=${encodeURIComponent(filters)}`),'_blank');
+    window.open(generatePath(this.context, `browser/${className}?filters=${encodeURIComponent(filters)}`),'_blank');
   }
 
   handleCLPChange(clp) {
@@ -969,7 +968,7 @@ class Browser extends DashboardView {
     this.setState({ rowsToDelete: null, selection: {} });
     let className = this.props.params.className;
     if (!this.state.relation && rows['*']) {
-      this.context.currentApp.clearCollection(className).then(() => {
+      this.context.clearCollection(className).then(() => {
         if (this.props.params.className === className) {
           this.state.counts[className] = 0;
           this.setState({
@@ -1272,7 +1271,7 @@ class Browser extends DashboardView {
     // get ordered list of class columns
     const columns = ColumnPreferences.getOrder(
       columnsObject,
-      this.context.currentApp.applicationId,
+      this.context.applicationId,
       className
     ).filter(column => column.visible);
 
@@ -1450,7 +1449,7 @@ class Browser extends DashboardView {
 
   async onChangeDefaultKey (name) {
     ColumnPreferences.setPointerDefaultKey(
-      this.context.currentApp.applicationId,
+      this.context.applicationId,
       this.props.params.className,
       name
       );
@@ -1514,6 +1513,7 @@ class Browser extends DashboardView {
         }
         browser = (
           <DataBrowser
+            app={this.context}
             ref={this.dataBrowserRef}
             isUnique={this.state.isUnique}
             uniqueField={this.state.uniqueField}
@@ -1580,7 +1580,7 @@ class Browser extends DashboardView {
       let currentColumns = this.getClassColumns(className).map(column => column.name);
       extras = (
         <PointerKeyDialog
-          app={this.context.currentApp}
+          app={this.context}
           className={className}
           currentColumns={currentColumns}
           onCancel={() => this.setState({ showPointerKeyDialog: false })}
@@ -1590,14 +1590,14 @@ class Browser extends DashboardView {
     else if (this.state.showCreateClassDialog) {
       extras = (
         <CreateClassDialog
-          currentAppSlug={this.context.currentApp.slug}
+          currentAppSlug={this.context.slug}
           onAddColumn={this.showAddColumn}
           currentClasses={this.props.schema.data.get('classes').keySeq().toArray()}
           onCancel={() => this.setState({ showCreateClassDialog: false })}
           onConfirm={this.createClass} />
       );
     } else if (this.state.showAddColumnDialog) {
-      const { currentApp = {} } = this.context;
+      const currentApp = this.context || {};
       let currentColumns = [];
       classes.get(className).forEach((field, name) => {
         currentColumns.push(name);
@@ -1683,10 +1683,10 @@ class Browser extends DashboardView {
         columnsObject[column.name] = column
       });
       // get ordered list of class columns
-      const columnPreferences = this.context.currentApp.columnPreference || {}
+      const columnPreferences = this.context.columnPreference || {}
       const columns = ColumnPreferences.getOrder(
         columnsObject,
-        this.context.currentApp.applicationId,
+        this.context.applicationId,
         className,
         columnPreferences[className]
       );
@@ -1773,7 +1773,3 @@ class Browser extends DashboardView {
     );
   }
 }
-
-Browser.contextTypes = {
-  currentApp: PropTypes.instanceOf(ParseApp)
-};

src/components/CategoryList/CategoryList.react.js
@@ -5,12 +5,15 @@
  * This source code is licensed under the license found in the LICENSE file in
  * the root directory of this source tree.
  */
-import PropTypes from 'lib/PropTypes';
-import React     from 'react';
-import styles    from 'components/CategoryList/CategoryList.scss';
-import { Link }  from 'react-router-dom';
+import PropTypes      from 'lib/PropTypes';
+import React          from 'react';
+import styles         from 'components/CategoryList/CategoryList.scss';
+import { Link }       from 'react-router-dom';
+import generatePath   from 'lib/generatePath';
+import { CurrentApp } from 'context/currentApp';
 
 export default class CategoryList extends React.Component {
+  static contextType = CurrentApp;
   constructor() {
     super();
     this.listWrapperRef = React.createRef();
@@ -64,7 +67,8 @@ export default class CategoryList extends React.Component {
           }
           let count = c.count;
           let className = id === this.props.current ? styles.active : '';
-          let link = this.context.generatePath(
+          let link = generatePath(
+            this.context,
             (this.props.linkPrefix || '') + (c.link || id)
           );
           return (
@@ -83,8 +87,4 @@ CategoryList.propTypes = {
   categories: PropTypes.arrayOf(PropTypes.object).describe('Array of categories used to populate list.'),
   current: PropTypes.string.describe('Id of current category to be highlighted.'),
   linkPrefix: PropTypes.string.describe('Link prefix used to generate link path.'),
-};
-
-CategoryList.contextTypes = {
-  generatePath: PropTypes.func
 };

src/components/Filter/Filter.react.js
@@ -5,12 +5,12 @@
  * This source code is licensed under the license found in the LICENSE file in
  * the root directory of this source tree.
  */
-import * as Filters  from 'lib/Filters';
-import { List, Map } from 'immutable';
-import PropTypes     from 'lib/PropTypes';
-import React         from 'react';
-import stringCompare from 'lib/stringCompare';
-import ParseApp      from 'lib/ParseApp';
+import * as Filters   from 'lib/Filters';
+import { List, Map }  from 'immutable';
+import PropTypes      from 'lib/PropTypes';
+import React          from 'react';
+import stringCompare  from 'lib/stringCompare';
+import { CurrentApp } from 'context/currentApp';
 
 function changeField(schema, filters, index, newField) {
   let newFilter = new Map({
@@ -44,7 +44,8 @@ function deleteRow(filters, index) {
   return filters.delete(index);
 }
 
-let Filter = ({ schema, filters, renderRow, onChange, blacklist, className }, context) => {
+let Filter = ({ schema, filters, renderRow, onChange, blacklist, className }) => {
+  const currentApp = React.useContext(CurrentApp);
   blacklist = blacklist || [];
   let available = Filters.availableFilters(schema, filters);
   return (
@@ -60,7 +61,7 @@ let Filter = ({ schema, filters, renderRow, onChange, blacklist, className }, co
         }
 
         // Get the column preference of the current class.
-        const currentColumnPreference = context.currentApp.columnPreference[className];
+        const currentColumnPreference = currentApp.columnPreference[className];
 
         // Check if the preference exists.
         if (currentColumnPreference) {
@@ -136,8 +137,4 @@ Filter.propTypes = {
   renderRow: PropTypes.func.isRequired.describe(
     'A function for rendering a row of a filter.'
   )
-};
-
-Filter.contextTypes = {
-  currentApp: PropTypes.instanceOf(ParseApp)
 };

src/components/Popover/Popover.react.js
@@ -5,11 +5,9 @@
  * This source code is licensed under the license found in the LICENSE file in
  * the root directory of this source tree.
  */
-import PropTypes        from 'lib/PropTypes';
 import hasAncestor      from 'lib/hasAncestor';
 import React            from 'react';
 import styles           from 'components/Popover/Popover.scss';
-import ParseApp         from 'lib/ParseApp';
 import { createPortal } from 'react-dom';
 
 // We use this component to proxy the current tree's context
@@ -94,9 +92,3 @@ export default class Popover extends React.Component {
     return createPortal(this.props.children, this._popoverLayer);
   }
 }
-
-Popover.contextTypes = {
-  history: PropTypes.object,
-  router: PropTypes.object,
-  currentApp: PropTypes.instanceOf(ParseApp)
-};

src/components/PushAudienceDialog/PushAudienceDialog.react.js
@@ -17,7 +17,6 @@ import Label                 from 'components/Label/Label.react';
 import Modal                 from 'components/Modal/Modal.react';
 import MultiSelect           from 'components/MultiSelect/MultiSelect.react';
 import MultiSelectOption     from 'components/MultiSelect/MultiSelectOption.react';
-import ParseApp              from 'lib/ParseApp';
 import PropTypes             from 'lib/PropTypes';
 import queryFromFilters      from 'lib/queryFromFilters';
 import React                 from 'react';
@@ -25,6 +24,7 @@ import styles                from 'components/PushAudienceDialog/PushAudienceDia
 import TextInput             from 'components/TextInput/TextInput.react';
 import Toggle                from 'components/Toggle/Toggle.react';
 import { List, Map }         from 'immutable';
+import { CurrentApp }        from 'context/currentApp';
 
 const PARSE_SERVER_SUPPORTS_SAVED_AUDIENCES = true;
 const AUDIENCE_SIZE_FETCHING_ENABLED = true;
@@ -44,6 +44,7 @@ let filterFormatter = (filters, schema) => {
 }
 
 export default class PushAudienceDialog extends React.Component {
+  static contextType = CurrentApp;
   constructor() {
     super();
     this.xhrHandle = null;
@@ -113,7 +114,7 @@ export default class PushAudienceDialog extends React.Component {
   }
 
   fetchAudienceSize() {
-    if (!this.context || !this.context.currentApp) { //so we don't break the PIG demo
+    if (!this.context) { //so we don't break the PIG demo
       return;
     }
 
@@ -124,7 +125,7 @@ export default class PushAudienceDialog extends React.Component {
       query = parseQuery.toJSON().where || {};
     }
     query.deviceType = { $in: this.state.platforms };
-    let {xhr, promise} = this.context.currentApp.fetchPushSubscriberCount(PushConstants.NEW_SEGMENT_ID, query);
+    let {xhr, promise} = this.context.fetchPushSubscriberCount(PushConstants.NEW_SEGMENT_ID, query);
     if (this.xhrHandle) { //cancel existing xhr - prevent from stacking
       this.xhrHandle.abort();
     }
@@ -271,10 +272,6 @@ export default class PushAudienceDialog extends React.Component {
     );
   }
 }
-
-PushAudienceDialog.contextTypes = {
-  currentApp: PropTypes.instanceOf(ParseApp)
-};
 
 PushAudienceDialog.propTypes = {
   editMode: PropTypes.bool.describe(

src/components/PushAudiencesSelector/PushAudiencesBaseRow.react.js
@@ -5,12 +5,12 @@
  * This source code is licensed under the license found in the LICENSE file in
  * the root directory of this source tree.
  */
-import PropTypes          from 'lib/PropTypes';
-import ParseApp           from 'lib/ParseApp';
 import React              from 'react';
 import { NEW_SEGMENT_ID } from 'dashboard/Push/PushConstants';
+import { CurrentApp }     from 'context/currentApp';
 
 export default class PushAudiencesBaseRow extends React.Component {
+  static contextType = CurrentApp;
   constructor() {
     super();
     this.xhrHandle = null;
@@ -31,12 +31,12 @@ export default class PushAudiencesBaseRow extends React.Component {
   }
 
   fetchPushSubscriberCount(context) {
-    if (!context || !context.currentApp) { //so we don't break the PIG demo
+    if (!context) { //so we don't break the PIG demo
       return;
     }
     let query = this.props.id === NEW_SEGMENT_ID ? this.props.query : null;
     //Added count fetch logic directly to component
-    let {xhr, promise} = context.currentApp.fetchPushSubscriberCount(this.props.id, query);
+    let {xhr, promise} = context.fetchPushSubscriberCount(this.props.id, query);
     this.xhrHandle = xhr;
     promise.then(({ approximate, count }) => {
       this.setState({ approximate, count });
@@ -62,7 +62,3 @@ export default class PushAudiencesBaseRow extends React.Component {
     }
   }
 }
-
-PushAudiencesBaseRow.contextTypes = {
-  currentApp: PropTypes.instanceOf(ParseApp)
-};

src/components/PushPreview/PushPreview.react.js
@@ -11,6 +11,7 @@ import React              from 'react';
 import SegmentSelect      from 'components/SegmentSelect/SegmentSelect.react';
 import styles             from 'components/PushPreview/PushPreview.scss';
 import VisiblePreview     from 'components/PushPreview/VisiblePreview.react';
+import { CurrentApp }     from 'context/currentApp';
 import {
          getDateMethod,
          MONTHS,
@@ -37,6 +38,7 @@ let timeString = (time, isLocal) => {
 }
 
 export default class PushPreview extends React.Component {
+  static contextType = CurrentApp;
   constructor(props) {
     super(props);
 
@@ -134,7 +136,7 @@ export default class PushPreview extends React.Component {
         type={this.state.currentPreview.toLowerCase().replace(/\s/, '')}
         message={previewMessage}
         time={previewTime || new Date()}
-        appName={this.context.currentApp.name}
+        appName={this.context.name}
         fade={isExperiment} />
     );
     if (!isExperiment && pushState.data_type === 'json') {
@@ -177,7 +179,3 @@ export default class PushPreview extends React.Component {
     );
   }
 }
-
-PushPreview.contextTypes = {
-  currentApp: PropTypes.instanceOf(ParseApp)
-};

src/components/Sidebar/Sidebar.react.js
@@ -5,19 +5,18 @@
  * This source code is licensed under the license found in the LICENSE file in
  * the root directory of this source tree.
  */
-import PropTypes   from 'lib/PropTypes';
 import AppsManager    from 'lib/AppsManager';
 import AppsMenu       from 'components/Sidebar/AppsMenu.react';
 import AppName        from 'components/Sidebar/AppName.react';
 import FooterMenu     from 'components/Sidebar/FooterMenu.react';
 import isInsidePopover from 'lib/isInsidePopover';
-import ParseApp       from 'lib/ParseApp';
 import Pin            from 'components/Sidebar/Pin.react';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import SidebarHeader  from 'components/Sidebar/SidebarHeader.react';
 import SidebarSection from 'components/Sidebar/SidebarSection.react';
 import SidebarSubItem from 'components/Sidebar/SidebarSubItem.react';
 import styles         from 'components/Sidebar/Sidebar.scss';
+import { CurrentApp } from 'context/currentApp';
 
 const Sidebar = ({
   prefix,
@@ -30,7 +29,8 @@ const Sidebar = ({
   appSelector,
   primaryBackgroundColor,
   secondaryBackgroundColor
-}, { currentApp }) => {
+}) => {
+  const currentApp = useContext(CurrentApp);
   const collapseWidth = 980;
   const [ appsMenuOpen, setAppsMenuOpen ] = useState(false);
   const [ collapsed, setCollapsed ] = useState(false);
@@ -191,9 +191,5 @@ const Sidebar = ({
     </div>
   );
 }
-
-Sidebar.contextTypes = {
-  currentApp: PropTypes.instanceOf(ParseApp)
-};
 
 export default Sidebar;

commit 13fe3d327b6ae1e72a737de2c70ed04707ebc6a3

fix: regressions caused by #1948 and #1866

src/components/FileEditor/FileEditor.react.js
@@ -78,7 +78,7 @@ export default class FileEditor extends React.Component {
   render() {
     const file = this.props.value;
     return (
-      <div ref={this.inputRef.current} style={{ minWidth: this.props.width, display: 'none' }} className={styles.editor}>
+      <div ref={this.inputRef} style={{ minWidth: this.props.width, display: 'none' }} className={styles.editor}>
         <a className={styles.upload}>
           <input ref={this.fileInputRef} id='fileInput' type='file' onChange={this.handleChange.bind(this)} />
           <span>{file ? 'Replace file' : 'Upload file'}</span>

src/dashboard/Data/Browser/Browser.react.js
@@ -167,7 +167,7 @@ class Browser extends DashboardView {
   }
 
   componentWillReceiveProps(nextProps, nextContext) {
-    if (this.props.params.className !== nextProps.params.className) {
+    if (this.props.params.appId !== nextProps.params.appId || this.props.params.className !== nextProps.params.className || this.props.location.search !== nextProps.location.search) {
       if (this.props.params.appId !== nextProps.params.appId || !this.props.params.className) {
         this.setState({ counts: {} });
         Parse.Object._clearAllState();
@@ -177,7 +177,7 @@ class Browser extends DashboardView {
       .then(() => this.handleFetchedSchema());
     }
     if (!nextProps.params.className && nextProps.schema.data.get('classes')) {
-      this.redirectToFirstClass(nextProps.schema.data.get('classes'));
+      this.redirectToFirstClass(nextProps.schema.data.get('classes'), nextContext);
     }
   }
 
@@ -225,7 +225,7 @@ class Browser extends DashboardView {
     return filters;
   }
 
-  redirectToFirstClass(classList) {
+  redirectToFirstClass(classList, context) {
     if (!classList.isEmpty()) {
       let classes = Object.keys(classList.toObject());
       classes.sort((a, b) => {
@@ -237,7 +237,7 @@ class Browser extends DashboardView {
         }
         return a.toUpperCase() < b.toUpperCase() ? -1 : 1;
       });
-      history.replace(generatePath(this.context, 'browser/' + classes[0]));
+      history.replace(generatePath(context || this.context, 'browser/' + classes[0]));
     }
   }
 