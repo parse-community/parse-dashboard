b4a4444d8416d347600b17c7730cede48ccb89ab
Damian Stasik <visualfanatic@users.noreply.github.com>
2022-01-30 10:17:04 +0100

    Drastically simplify cypress setup by using single server with request interception

--- cypress/integration/apps-index.spec.js
+++ cypress/integration/apps-index.spec.js
@@ -1,14 +1,77 @@
 /// <reference types="cypress" />
 
-describe('Apps index', () => {
-  it('Redirects to app lists from root path', () => {
-    cy.visit('http://localhost:4050')
+function getDashboardConfig(apps) {
+  return {
+    newFeaturesInLatestVersion: [],
+    apps: apps.map(app => ({
+      serverURL: "http://localhost:1337/parse",
+      appId: "hello",
+      masterKey: app.masterKey || "world",
+      appName: app.appName,
+      iconName: app.iconName || "",
+      primaryBackgroundColor: app.primaryBackgroundColor || "",
+      secondaryBackgroundColor: app.secondaryBackgroundColor || "",
+    })),
+  };
+}
 
-    cy.url().should('match', /\/apps$/)
-  })
+describe('Apps index', () => {
+  it('Redirects to app lists from root path if there are 2 or more apps', () => {
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/parse-dashboard-config.json',
+      },
+      getDashboardConfig([
+        {
+          appName: 'test1',
+        },
+        {
+          appName: 'test2',
+        },
+      ]),
+    );
+
+    cy.visit('/');
+
+    cy.url().should('match', /\/apps$/);
+  });
+
+  it('Redirects to first app if there is only one', () => {
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/parse-dashboard-config.json',
+      },
+      getDashboardConfig([
+        {
+          appName: 'test1',
+        },
+      ]),
+    );
+
+    cy.visit('/');
+
+    cy.url().should('match', /\/apps\/test1\/browser\/_Role$/);
+  });
 
   it('Can filter apps with search input', () => {
-    cy.visit('http://localhost:4050/apps')
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/parse-dashboard-config.json',
+      },
+      getDashboardConfig([
+        {
+          appName: 'test1',
+        },
+        {
+          appName: 'test2',
+        },
+      ]),
+    );
+
+    cy.visit('/apps')
 
     cy.get('[data-test-id="apps-index-app"]').should('have.length', 2)
 
