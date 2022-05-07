aba50cafd2c93ffce950957449ab738cab559fb3
Bugra <2475497+bugra9@users.noreply.github.com>
2022-05-07 22:22:00 +0300

    refactor dashboard.e2e.test.js

--- src/lib/tests/e2e/dashboard.e2e.test.js
+++ src/lib/tests/e2e/dashboard.e2e.test.js
@@ -50,50 +50,34 @@ describe('dashboard e2e', () => {
 });
 
 
-describe('port, config, appId, serverURL, masterKey, appName, graphQLServerURL options should not be ignored.', () => {
-  it('Should start with port 4041.', async () => {
+describe('Config options', () => {
+  it('should start with port option', async () => {
     const result = await startParseDashboardAndGetOutput(['--port', '4041']);
-
     expect(result).toContain('The dashboard is now available at http://0.0.0.0:4041/');
   });
 
-  it('Should return an error message if config and appId options are provided together.', async () => {
-    const result = await startParseDashboardAndGetOutput(['--config', 'helloworld', '--appId', 'helloworld']);
+  it('should reject to start if config and other options are combined', async () => {
+    const args = [
+      '--appId',
+      '--serverURL',
+      '--masterKey',
+      '--appName',
+      '--graphQLServerURL'
+    ];
 
-    expect(result).toContain('You must provide either a config file or other CLI options (appName, appId, masterKey, serverURL, and graphQLServerURL); not both.');
-  });
-
-  it('Should return an error message if config and serverURL options are provided together.', async () => {
-    const result = await startParseDashboardAndGetOutput(['--config', 'helloworld', '--serverURL', 'helloworld']);
-
-    expect(result).toContain('You must provide either a config file or other CLI options (appName, appId, masterKey, serverURL, and graphQLServerURL); not both.');
-  });
-
-  it('Should return an error message if config and masterKey options are provided together.', async () => {
-    const result = await startParseDashboardAndGetOutput(['--config', 'helloworld', '--masterKey', 'helloworld']);
-
-    expect(result).toContain('You must provide either a config file or other CLI options (appName, appId, masterKey, serverURL, and graphQLServerURL); not both.');
-  });
-
-  it('Should return an error message if config and appName options are provided together.', async () => {
-    const result = await startParseDashboardAndGetOutput(['--config', 'helloworld', '--appName', 'helloworld']);
-
-    expect(result).toContain('You must provide either a config file or other CLI options (appName, appId, masterKey, serverURL, and graphQLServerURL); not both.');
-  });
-
-  it('Should return an error message if config and graphQLServerURL options are provided together.', async () => {
-    const result = await startParseDashboardAndGetOutput(['--config', 'helloworld', '--graphQLServerURL', 'helloworld']);
-
-    expect(result).toContain('You must provide either a config file or other CLI options (appName, appId, masterKey, serverURL, and graphQLServerURL); not both.');
+    for (const arg of args) {
+      const result = await startParseDashboardAndGetOutput(['--config', 'helloworld', arg, 'helloworld']);
+      expect(result).toContain('You must provide either a config file or other CLI options (appName, appId, masterKey, serverURL, and graphQLServerURL); not both.');
+    }
   });
 });
 
 function startParseDashboardAndGetOutput(args) {
-  const TIMEOUT = 1000; // ms
+  const timeoutInMs = 1000;
   return new Promise((resolve) => {
     const indexFilePath = path.resolve('./Parse-Dashboard/index.js');
-    const child = spawn('node', [indexFilePath, ...args], { cwd: '.', timeout: TIMEOUT });
-    setTimeout(() => { child.kill(); }, TIMEOUT); // node.js 12 hack (spawn timeout option is not supported.)
+    const child = spawn('node', [indexFilePath, ...args], { cwd: '.', timeout: timeoutInMs });
+    setTimeout(() => { child.kill(); }, timeoutInMs); // node.js 12 hack (spawn timeout option is not supported.)
 
     let output = '';
     child.on('error', () => { resolve(output); });
