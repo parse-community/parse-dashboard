89cad048eaf808a9d1094467c269688c06a62889
dblythy <daniel-blyth@live.com.au>
2022-09-17 17:54:10 +1000

    change color

--- src/components/ContextMenu/ContextMenu.react.js
+++ src/components/ContextMenu/ContextMenu.react.js
@@ -75,6 +75,7 @@ const MenuSection = ({ level, items, path, setPath, hide }) => {
             }}
           >
             {item.text}
+            {item.subtext && <span style={{color: '#A2A6B1'}}> - {item.subtext}</span>}
           </li>
         );
     })}
