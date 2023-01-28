164f35a3cdadc2fed6544f80f5aee93f15448ad8
dblythy <daniel-blyth@live.com.au>
2023-01-28 11:52:33 +1100

    Update DashboardView.react.js

--- src/dashboard/DashboardView.react.js
+++ src/dashboard/DashboardView.react.js
@@ -34,7 +34,9 @@ export default class DashboardView extends React.Component {
   onRouteChanged() {
     const appId = this.context.applicationId;
     const path = this.props.location?.pathname ?? window.location.pathname;
+    console.log({path, appId});
     const route = path.split(appId)[1].split('/')[1];
+    console.log({route});
     if (route !== this.state.route) {
       this.setState({ route });
     }
