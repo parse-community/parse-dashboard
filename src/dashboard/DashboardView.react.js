e11fa342947b6993d7439b8e799f88b6d5c80281
Josh T <72992663+jpthurman@users.noreply.github.com>
2023-02-02 06:43:25 -0600

    Update DashboardView.react.js

--- src/dashboard/DashboardView.react.js
+++ src/dashboard/DashboardView.react.js
@@ -199,7 +199,7 @@ export default class DashboardView extends React.Component {
 
     if (coreSubsections.length > 0) {
       appSidebarSections.push({
-        name: 'Core',
+        name: 'Core test',
         icon: 'core',
         link: '/browser',
         subsections: coreSubsections,
