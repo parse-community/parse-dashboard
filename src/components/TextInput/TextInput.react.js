313261a8749572232290115dfb72080333805ab4
Ashish Baravaliya <ashishbaravaliya@Ashishs-Air.cgocable.net>
2023-04-14 10:32:50 -0400

    fix: remove code of another bug fix

--- src/components/TextInput/TextInput.react.js
+++ src/components/TextInput/TextInput.react.js
@@ -15,7 +15,6 @@ class TextInput extends React.Component {
     if (props.multiline !== this.props.multiline) {
       const node = props.forwardedRef.current;
       node.focus();
-      node.setSelectionRange(this.props.value.length, this.props.value.length);
     }
   }
 
