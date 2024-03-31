425c37d42c6c27ec7edd5cf00465bbedc6fb2c3a
Milan Patel <patelmilanun@gmail.com>
2024-04-01 03:05:21 +0530

    fix: build serving via express

--- v2/vite.config.ts
+++ v2/vite.config.ts
@@ -5,8 +5,14 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../Parse-Dashboard/v2',
+    outDir: '../Parse-Dashboard/public/v2',
     emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].bundle.js',
+        assetFileNames: '[name].bundle.[ext]',
+      },
+    },
   },
   base: '/v2/',
 });
