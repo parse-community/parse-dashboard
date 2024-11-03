c3950d19c12435e5af996785bb48e43b96ad73f7
unknown <harsh0604@gmail.com>
2024-11-04 03:18:58 +0530

    undo panel width size changess

--- src/dashboard/Data/Browser/DataBrowser.react.js
+++ src/dashboard/Data/Browser/DataBrowser.react.js
@@ -45,7 +45,7 @@ export default class DataBrowser extends React.Component {
       firstSelectedCell: null,
       selectedData: [],
       prevClassName: props.className,
-      panelWidth: 400,
+      panelWidth: 300,
       isResizing: false,
       maxWidth: window.innerWidth - 300,
       showAggregatedData: true,
@@ -591,7 +591,7 @@ export default class DataBrowser extends React.Component {
             <ResizableBox
               width={this.state.panelWidth}
               height={Infinity}
-              minConstraints={[400, Infinity]}
+              minConstraints={[100, Infinity]}
               maxConstraints={[this.state.maxWidth, Infinity]}
               onResizeStart={this.handleResizeStart} // Handle start of resizing
               onResizeStop={this.handleResizeStop} // Handle end of resizing
