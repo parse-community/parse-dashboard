79040d7e4475fa1d391a742aaf722759e291a2bb
Manuel <5673677+mtrezza@users.noreply.github.com>
2025-07-09 14:25:43 +0200

    fix frozen row background

--- src/components/BrowserCell/BrowserCell.react.js
+++ src/components/BrowserCell/BrowserCell.react.js
@@ -637,7 +637,6 @@ export default class BrowserCell extends Component {
       style.position = 'sticky';
       style.left = this.props.stickyLeft;
       style.zIndex = 1;
-      style.background = this.props.rowBackground;
       style.borderBottom = '1px solid #e3e3ea';
     }
 
