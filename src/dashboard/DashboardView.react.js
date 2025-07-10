1708ad9450da7559c27026e7d8240e1ac67a4527
Manuel <5673677+mtrezza@users.noreply.github.com>
2025-07-11 01:46:49 +0200

    fix: allow route for views page

--- src/dashboard/DashboardView.react.js
+++ src/dashboard/DashboardView.react.js
@@ -306,9 +306,10 @@ export default class DashboardView extends React.Component {
     );
 
     let content = <div className={styles.content}>{this.renderContent()}</div>;
-    const canRoute = [...coreSubsections, ...pushSubsections, ...settingsSections]
-      .map(({ link }) => link.split('/')[1])
-      .includes(this.state.route);
+    const allowedRoutes = [...coreSubsections, ...pushSubsections, ...settingsSections]
+      .map(({ link }) => link.split('/')[1]);
+    const canRoute =
+      allowedRoutes.includes(this.state.route) || this.state.route === 'views';
 
     if (!canRoute) {
       content = (
