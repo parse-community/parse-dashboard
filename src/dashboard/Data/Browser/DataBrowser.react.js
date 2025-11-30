522f4a2232d2a9a790391a68a40407dadaee0cc4
Manuel Trezza <5673677+mtrezza@users.noreply.github.com>
2025-11-30 19:55:19 +0100

    panel count

--- src/dashboard/Data/Browser/DataBrowser.react.js
+++ src/dashboard/Data/Browser/DataBrowser.react.js
@@ -27,6 +27,7 @@ const AGGREGATION_PANEL_SYNC_SCROLL = 'aggregationPanelSyncScroll';
 const AGGREGATION_PANEL_BATCH_NAVIGATE = 'aggregationPanelBatchNavigate';
 const AGGREGATION_PANEL_SHOW_CHECKBOX = 'aggregationPanelShowCheckbox';
 const AGGREGATION_PANEL_WIDTH = 'aggregationPanelWidth';
+const AGGREGATION_PANEL_COUNT = 'aggregationPanelCount';
 
 function formatValueForCopy(value, type) {
   if (value === undefined) {
@@ -101,6 +102,8 @@ export default class DataBrowser extends React.Component {
       window.localStorage?.getItem(AGGREGATION_PANEL_SHOW_CHECKBOX) !== 'false';
     const storedPanelWidth = window.localStorage?.getItem(AGGREGATION_PANEL_WIDTH);
     const parsedPanelWidth = storedPanelWidth ? parseInt(storedPanelWidth, 10) : 300;
+    const storedPanelCount = window.localStorage?.getItem(AGGREGATION_PANEL_COUNT);
+    const parsedPanelCount = storedPanelCount ? parseInt(storedPanelCount, 10) : 1;
     const hasAggregation =
       props.classwiseCloudFunctions?.[
         `${props.app.applicationId}${props.appName}`
@@ -134,7 +137,7 @@ export default class DataBrowser extends React.Component {
       prefetchCache: {},
       selectionHistory: [],
       displayedObjectIds: [], // Array of object IDs currently displayed in the panel
-      panelCount: 1, // Number of panels to display
+      panelCount: parsedPanelCount, // Number of panels to display
       multiPanelData: {}, // Object mapping objectId to panel data
       _objectsToFetch: [], // Temporary field for async fetch handling
       loadingObjectIds: new Set(),
@@ -1152,6 +1155,8 @@ export default class DataBrowser extends React.Component {
         multiPanelData: currentObjectData,
         panelWidth: limitedWidth,
       });
+      window.localStorage?.setItem(AGGREGATION_PANEL_COUNT, newPanelCount);
+      window.localStorage?.setItem(AGGREGATION_PANEL_WIDTH, limitedWidth);
 
       // Fetch missing data asynchronously
       objectsToFetch.forEach((objectId, i) => {
@@ -1172,6 +1177,9 @@ export default class DataBrowser extends React.Component {
       const newDisplayedObjectIds = prevState.displayedObjectIds.slice(0, -1);
 
       const newWidth = (prevState.panelWidth / prevState.panelCount) * newPanelCount;
+
+      window.localStorage?.setItem(AGGREGATION_PANEL_COUNT, newPanelCount);
+      window.localStorage?.setItem(AGGREGATION_PANEL_WIDTH, newWidth);
 
       return {
         panelCount: newPanelCount,
