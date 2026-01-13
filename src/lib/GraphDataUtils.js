29ae3c539846d819b36c971826902735aa2ad094
Manuel Trezza <5673677+mtrezza@users.noreply.github.com>
2026-01-13 18:59:15 +0100

    fix

--- src/lib/GraphDataUtils.js
+++ src/lib/GraphDataUtils.js
@@ -129,11 +129,11 @@ export function formatDateCompact(date) {
     return '';
   }
 
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
-  const hours = String(date.getHours()).padStart(2, '0');
-  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const year = date.getUTCFullYear();
+  const month = String(date.getUTCMonth() + 1).padStart(2, '0');
+  const day = String(date.getUTCDate()).padStart(2, '0');
+  const hours = String(date.getUTCHours()).padStart(2, '0');
+  const minutes = String(date.getUTCMinutes()).padStart(2, '0');
 
   return `${year}-${month}-${day} ${hours}:${minutes}`;
 }
