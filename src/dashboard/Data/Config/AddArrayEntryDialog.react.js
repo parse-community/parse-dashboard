7b5d3bb6d8a93106ab7ccf84059b6d8fa6bd6d15
Manuel Trezza <5673677+mtrezza@users.noreply.github.com>
2026-01-17 18:47:57 +0100

    fix

--- src/dashboard/Data/Config/AddArrayEntryDialog.react.js
+++ src/dashboard/Data/Config/AddArrayEntryDialog.react.js
@@ -17,7 +17,7 @@ export default class AddArrayEntryDialog extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: props.initialValue || '',
+      value: String(props.initialValue ?? ''),
       showMismatchRow: false,
       mismatchConfirmed: false,
       parsedType: '',
@@ -28,6 +28,18 @@ export default class AddArrayEntryDialog extends React.Component {
   componentDidMount() {
     if (this.inputRef.current) {
       this.inputRef.current.focus();
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    // Sync state when initialValue prop changes
+    if (prevProps.initialValue !== this.props.initialValue) {
+      this.setState({
+        value: String(this.props.initialValue ?? ''),
+        showMismatchRow: false,
+        mismatchConfirmed: false,
+        parsedType: '',
+      });
     }
   }
 
