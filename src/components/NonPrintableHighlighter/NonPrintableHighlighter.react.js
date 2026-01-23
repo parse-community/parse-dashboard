a5b5ee2c3327b0506a22da172fc34ef16794a759
Manuel Trezza <5673677+mtrezza@users.noreply.github.com>
2026-01-23 15:45:26 +0100

    lint

--- src/components/NonPrintableHighlighter/NonPrintableHighlighter.react.js
+++ src/components/NonPrintableHighlighter/NonPrintableHighlighter.react.js
@@ -150,7 +150,7 @@ function stripQuotes(str) {
     return str;
   }
   // Check for matching single or double quotes
-  if ((str.startsWith('"') && str.endsWith('"')) || (str.startsWith("'") && str.endsWith("'"))) {
+  if ((str.startsWith('"') && str.endsWith('"')) || (str.startsWith('\'') && str.endsWith('\''))) {
     return str.slice(1, -1);
   }
   return str;
