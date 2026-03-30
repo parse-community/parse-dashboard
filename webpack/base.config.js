23a1aafb3abef436f75c6c7461dc50fb44674612
Manuel <5673677+mtrezza@users.noreply.github.com>
2026-03-30 11:27:06 +0100

    refactor: Set namedExport to false for css-loader v7 compatibility
    In css-loader v7, modules.namedExport defaults to true when esModule
    is enabled. This changes how CSS module classes are exported, breaking
    the existing import pattern `import styles from './Component.scss'`.
    Setting namedExport to false maintains backward compatibility.

--- webpack/base.config.js
+++ webpack/base.config.js
@@ -48,7 +48,9 @@ module.exports = {
           {
             loader: 'css-loader',
             options: {
-              modules: true,
+              modules: {
+                namedExport: false,
+              },
               importLoaders: 2,
             },
           },
